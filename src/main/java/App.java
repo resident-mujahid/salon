ca3937ed9442adddc4cf898e5cce8b363ef44c91
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-23 22:02:56 -0800

    Refactor routing using different action URLs instead of flags

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,7 +20,6 @@ public class App {
         model.put("stylist", Stylist.class);
         model.put("stylists", Stylist.all(true));
         model.put("unassignedclients", Client.unassignedClientsExist());
-
         model.put("template", "templates/index.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
@@ -34,7 +33,6 @@ public class App {
 
         model.put("stylist", thisStylist);
         model.put("clients", thisStylist.getAllClients());
-
         model.put("template", "templates/stylist.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
@@ -48,122 +46,150 @@ public class App {
         model.put("stylist", Stylist.class);
         model.put("currentstylist", currentStylist);
         model.put("client", thisClient);
-
         model.put("template", "templates/client.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
 
       //ROUTES: CHANGING RESOURCES
 
-      post("/", (request, response) -> {
+      //Create a stylist
+      post("/new-stylist", (request, response) -> {
         HashMap<String, Object> model = new HashMap<String, Object>();
 
-        //Create a stylist
-        if (request.queryParams("addnewstylist") != null) {
-          String requestedFirst = request.queryParams("firstname");
-          String requestedLast = request.queryParams("lastname");
-          Stylist requestedStylist = new Stylist(requestedFirst, requestedLast);
-          boolean duplicateStylistRequested = requestedStylist.isDuplicate();
-          if (!(duplicateStylistRequested)) {
-            requestedStylist.save();
-          } else {
-            model.put("duplicatestylistrequested", duplicateStylistRequested);
-          }
-        }
-
-        //Delete a stylist
-        if (request.queryParams("removestylist") != null) {
-          Stylist removalRequest = Stylist.find(Integer.parseInt(
-            request.queryParams("removestylist")));
-          removalRequest.delete();
+        String requestedFirst = request.queryParams("firstname");
+        String requestedLast = request.queryParams("lastname");
+        Stylist requestedStylist = new Stylist(requestedFirst, requestedLast);
+        boolean duplicateStylistRequested = requestedStylist.isDuplicate();
+        if (!(duplicateStylistRequested)) {
+          requestedStylist.save();
+        } else {
+          model.put("duplicatestylistrequested", duplicateStylistRequested);
         }
 
         model.put("client", Client.class);
         model.put("stylist", Stylist.class);
         model.put("stylists", Stylist.all(true));
         model.put("unassignedclients", Client.unassignedClientsExist());
-
         model.put("template", "templates/index.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
 
-      post("/stylists/:id", (request, response) -> {
+      //Delete a stylist
+      post("/remove-stylist", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+
+        Stylist removalRequest = Stylist.find(Integer.parseInt(
+          request.queryParams("removestylist")));
+        removalRequest.delete();
+
+        model.put("client", Client.class);
+        model.put("stylist", Stylist.class);
+        model.put("stylists", Stylist.all(true));
+        model.put("unassignedclients", Client.unassignedClientsExist());
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      //Update a stylist
+      post("/stylists/:id/update", (request, response) -> {
         HashMap<String, Object> model = new HashMap<String, Object>();
         Stylist thisStylist = Stylist.find(
           Integer.parseInt(request.params("id")));
 
-        //Update a stylist
-
-        if (request.queryParams("changestylist") != null) {
-          String requestedFirst = request.queryParams("newfirstname");
-          String requestedLast = request.queryParams("newlastname");
-          Stylist duplicateChecker = new Stylist(requestedFirst, requestedLast);
-          boolean duplicateStylistRequested = duplicateChecker.isDuplicate();
-          if (!(duplicateStylistRequested)) {
-            thisStylist.update(requestedFirst, requestedLast);
-          } else {
-            model.put("duplicatestylistrequested", duplicateStylistRequested);
-          }
-        }
-
-        //Create a client
-        if (request.queryParams("addnewclient") != null) {
-          String requestedFirst = request.queryParams("firstname");
-          String requestedLast = request.queryParams("lastname");
-          Client requestedClient = new Client(requestedFirst, requestedLast);
-          boolean duplicateClientRequested = requestedClient.isDuplicate();
-          if (!(duplicateClientRequested)) {
-            requestedClient.save();
-            requestedClient.assignStylist(thisStylist.getId());
-          } else {
-            model.put("duplicateclientrequested", duplicateClientRequested);
-          }
-        }
-
-        //Delete a client
-        if (request.queryParams("removeclient") != null) {
-          Client removalRequest = Client.find(Integer.parseInt(
-            request.queryParams("removeclient")));
-          removalRequest.delete();
+        String requestedFirst = request.queryParams("newfirstname");
+        String requestedLast = request.queryParams("newlastname");
+        Stylist duplicateChecker = new Stylist(requestedFirst, requestedLast);
+        boolean duplicateStylistRequested = duplicateChecker.isDuplicate();
+        if (!(duplicateStylistRequested)) {
+          thisStylist.update(requestedFirst, requestedLast);
+        } else {
+          model.put("duplicatestylistrequested", duplicateStylistRequested);
         }
 
         model.put("stylist", thisStylist);
         model.put("clients", thisStylist.getAllClients());
-
         model.put("template", "templates/stylist.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
 
-      post("/clients/:id", (request, response) -> {
+      //Create a client
+      post("/stylists/:id/new-client", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        Stylist thisStylist = Stylist.find(
+          Integer.parseInt(request.params("id")));
+
+        String requestedFirst = request.queryParams("firstname");
+        String requestedLast = request.queryParams("lastname");
+        Client requestedClient = new Client(requestedFirst, requestedLast);
+        boolean duplicateClientRequested = requestedClient.isDuplicate();
+        if (!(duplicateClientRequested)) {
+          requestedClient.save();
+          requestedClient.assignStylist(thisStylist.getId());
+        } else {
+          model.put("duplicateclientrequested", duplicateClientRequested);
+        }
+
+        model.put("stylist", thisStylist);
+        model.put("clients", thisStylist.getAllClients());
+        model.put("template", "templates/stylist.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      //Delete a client
+      post("/stylists/:id/remove-client", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        Stylist thisStylist = Stylist.find(
+          Integer.parseInt(request.params("id")));
+
+        Client removalRequest = Client.find(Integer.parseInt(
+            request.queryParams("removeclient")));
+        removalRequest.delete();
+
+        model.put("stylist", thisStylist);
+        model.put("clients", thisStylist.getAllClients());
+        model.put("template", "templates/stylist.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      //Update a client - change name
+      post("/clients/:id/update", (request, response) -> {
         HashMap<String, Object> model = new HashMap<String, Object>();
         Client thisClient = Client.find(
           Integer.parseInt(request.params("id")));
         Stylist currentStylist = Stylist.find(thisClient.getStylistId());
 
-        //Update a client - change name
-        if (request.queryParams("changeclientname") != null) {
-          String requestedFirst = request.queryParams("newfirstname");
-          String requestedLast = request.queryParams("newlastname");
-          Client duplicateChecker = new Client(requestedFirst, requestedLast);
-          boolean duplicateClientRequested = duplicateChecker.isDuplicate();
-          if (!(duplicateClientRequested)) {
-            thisClient.update(requestedFirst, requestedLast);
-          } else {
-            model.put("duplicateclientrequested", duplicateClientRequested);
-          }
-        }
-
-       //Update a client - change stylist
-        if (request.queryParams("changestylist") != null) {
-          thisClient.assignStylist(Integer.parseInt(
-            request.queryParams("newstylistid")));
-          currentStylist = Stylist.find(thisClient.getStylistId());
+        String requestedFirst = request.queryParams("newfirstname");
+        String requestedLast = request.queryParams("newlastname");
+        Client duplicateChecker = new Client(requestedFirst, requestedLast);
+        boolean duplicateClientRequested = duplicateChecker.isDuplicate();
+        if (!(duplicateClientRequested)) {
+          thisClient.update(requestedFirst, requestedLast);
+        } else {
+          model.put("duplicateclientrequested", duplicateClientRequested);
         }
 
         model.put("stylist", Stylist.class);
         model.put("currentstylist", currentStylist);
         model.put("client", thisClient);
+        model.put("template", "templates/client.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
+      //Update a client - change stylist
+      post("/clients/:id/assign-stylist", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        Client thisClient = Client.find(
+          Integer.parseInt(request.params("id")));
+        Stylist currentStylist = Stylist.find(thisClient.getStylistId());
+
+
+        thisClient.assignStylist(Integer.parseInt(
+          request.queryParams("newstylistid")));
+        currentStylist = Stylist.find(thisClient.getStylistId());
+
+        model.put("stylist", Stylist.class);
+        model.put("currentstylist", currentStylist);
+        model.put("client", thisClient);
         model.put("template", "templates/client.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
