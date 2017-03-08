7e334d028c8614954ae2aaa0f93d4645f700d031
Resident Mujahid <mujahidexpert@gmail.com>
2017-03-08 17:08:14 +0300

    made final changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,17 +8,27 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-     port(getHerokuAssignedPort());
+     // port(getHerokuAssignedPort());
       staticFileLocation("/public");
       String layout = "templates/layout.vtl";
 
-      static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+        ProcessBuilder process = new ProcessBuilder();
+          Integer port;
+          if (process.environment().get("PORT") != null) {
+              port = Integer.parseInt(process.environment().get("PORT"));
+          } else {
+              port = 4567;
+          }
+
+          setPort(port);
+
+    //   static int getHerokuAssignedPort() {
+    //     ProcessBuilder processBuilder = new ProcessBuilder();
+    //     if (processBuilder.environment().get("PORT") != null) {
+    //         return Integer.parseInt(processBuilder.environment().get("PORT"));
+    //     }
+    //     return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    // }
 
 
       //ROUTES: GETTING HOME PAGE
